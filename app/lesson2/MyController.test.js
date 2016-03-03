ada812a4aad5cb92336c7fc698e07513a18d2b10
Ron Apelbaum <ron.apelbaum@hpe.com>
2016-03-03 22:49:32 +0200

    improve lesson 2

--- app/lesson2/MyController.test.js
+++ app/lesson2/MyController.test.js
@@ -1,10 +1,8 @@
 /**
  * Created by apelbaur on 3/3/2016.
  */
-describe('MyController test suite', function () {
-    //angular services
+describe('MyController test suite: use inject()', function () {
     var $controller, MyDataService;
-
     beforeEach(module('lesson2'));
 
     beforeEach(inject(function (_$controller_, _MyDataService_) {
@@ -14,6 +12,32 @@ describe('MyController test suite', function () {
         }
     ));
 
+    var ctrl;
+    beforeEach(function () {
+        ctrl = $controller("MyController");
+    });
+
+    it("spyOn() injected service", function () {
+        spyOn(MyDataService, 'getName');
+        ctrl.greet();
+        expect(MyDataService.getName).toHaveBeenCalled();
+    });
+
+    it("spyOn() and return 'alice", function () {
+        spyOn(MyDataService, 'getName').and.returnValue('alice');
+        expect(ctrl.greet()).toBe('hello alice');
+    });
+});
+
+describe('MyController test suite: use simple DI', function () {
+    var $controller;
+    beforeEach(module('lesson2'));
+
+    beforeEach(inject(function (_$controller_) {
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+    }));
+
     /**
      * inject dependencies and instance a controller (use angular's DI)
      */
@@ -21,13 +45,44 @@ describe('MyController test suite', function () {
         return $controller('MyController', dependencies);
     }
 
-    it('test greet', function () {
-        var ctrl = getController({});
-        spyOn(MyDataService, 'getName').and.returnValue('alice');
+    it("test greet() - expect spyObj.getName() to have been called", function () {
+        var spyObj = jasmine.createSpyObj('MockService', ['getName']);
+        var ctrl = getController({
+            MyDataService: spyObj
+        });
 
-        var actual = ctrl.greet();
-
-        expect(actual).toBe('hello alice');
+        ctrl.greet();
+        expect(spyObj.getName).toHaveBeenCalled();
     });
 
+    it("test greet() - mock spyObj.getName() to return 'alice", function () {
+        var spyObj = jasmine.createSpyObj('MockService', ['getName']);
+        spyObj.getName.and.returnValue('alice');
+
+        var ctrl = getController({
+            MyDataService: spyObj
+        });
+        expect(ctrl.greet()).toBe('hello alice');
+    });
+});
+
+
+describe('MyController test suite: use $provide()', function () {
+    var $controller;
+    var spyObj = jasmine.createSpyObj('MockService', ['getName']);
+    spyObj.getName.and.returnValue('alice');
+
+    beforeEach(module('lesson2'));
+
+    beforeEach(module(function ($provide) {
+        $provide.value('MyDataService', spyObj);
+    }));
+
+    beforeEach(inject(function (_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    it("spyOn() and return 'alice", function () {
+        expect($controller("MyController").greet()).toBe('hello alice');
+    });
 });
